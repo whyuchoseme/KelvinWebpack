223d19b8bc52ef063e08e436783195e6a6044a70
andrey <no_questions.dota2@mail.ru>
2024-10-06 19:49:05 +0300

    first commit

---
+++ js/main.86e5011db65fecf90e04.js
@@ -0,0 +1,60 @@
+/******/ (() => { // webpackBootstrap
+/******/ 	"use strict";
+
+;// ./src/index.html
+// Imports
+var ___HTML_LOADER_IMPORT_0___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_1___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_2___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_3___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_4___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_5___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_6___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_7___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_8___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_9___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_10___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_11___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_12___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_13___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_14___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+var ___HTML_LOADER_IMPORT_15___ = (/* unused pure expression or super */ null && (/* unused asset import */ undefined));
+// Module
+var code = (/* unused pure expression or super */ null && (`<!DOCTYPE html> <html lang="ru"> <head> <meta charset="UTF-8"/> <meta name="viewport" content="width=device-width,initial-scale=1"/> <title>Kelvin Kramer</title> <link rel="shortcut icon" href="${___HTML_LOADER_IMPORT_0___}" type="image/x-icon"/> </head> <body> <header class="header"> <div class="container"> <nav class="header__navigation"> <a class="navigation-logo" href="index.html">Kelvin</a> <ul class="navigation-list"> <li class="navigation-item"> <a class="navigation-link active-link" href="#!">Home</a> </li> <li class="navigation-item"> <a class="navigation-link" href="#!">Works</a> </li> <li class="navigation-item"> <a class="navigation-link" href="#!">Services</a> </li> <li class="navigation-item"> <a class="navigation-link" href="#!">About</a> </li> <li class="navigation-item"> <a class="navigation-link" href="#!">Contact</a> </li> </ul> <button class="navigation-burger-button"><span></span></button> </nav> <div class="header-content"> <div class="header-content__info"> <h1>Kelvin Kramer <span>Designer & Developer</span></h1> <p> I’m a portrait, fashion and lifestyle photographer living In New York City. <span>During my thirteen year tenure here.</span> </p> </div> <picture> <source media="(max-width: 1024px)" srcset="${___HTML_LOADER_IMPORT_1___}"/> <img class="header-content__img" src="${___HTML_LOADER_IMPORT_2___}" alt="header-img"/> </picture> </div> </div> </header> <main class="main"> <section class="services"> <div class="container"> <div class="services-items"> <div class="services-card"> <img class="services-items__img" src="${___HTML_LOADER_IMPORT_3___}" alt="UI/UX Design"/> <h3>UI/UX Design</h3> <p> Our design is translated into comprehensive digital environments built on latest development standards. </p> </div> <div class="services-card"> <img class="services-items__img" src="${___HTML_LOADER_IMPORT_4___}" alt="Development"/> <h3>Development</h3> <p> Our design is translated into comprehensive digital environments built on latest development standards. </p> </div> <div class="services-card"> <img class="services-items__img" src="${___HTML_LOADER_IMPORT_5___}" alt="Software testing"/> <h3>Software testing</h3> <p> Our design is translated into comprehensive digital environments built on latest development standards. </p> </div> </div> </div> </section> <section class="portfolio"> <div class="container"> <h2 class="title">Portfolio</h2> <div class="portfolio-items"> <div class="portfolio-card"> <picture> <source media="(max-width: 768px)" srcset="${___HTML_LOADER_IMPORT_6___}"/> <img class="portfolio-items__img" src="${___HTML_LOADER_IMPORT_7___}" alt="Online fashion store - Homepage"/> </picture> <h3><a href="#!">Online fashion store - Homepage</a></h3> </div> </div> <div class="portfolio-items"> <div class="portfolio-card"> <picture> <source media="(max-width: 768px)" srcset="${___HTML_LOADER_IMPORT_8___}"/> <img class="portfolio-items__img" src="${___HTML_LOADER_IMPORT_9___}" alt="Reebok Store - Concept"/> </picture> <h3><a href="#!">Reebok Store - Concept</a></h3> </div> </div> <div class="portfolio-items"> <div class="portfolio-card"> <picture> <source media="(max-width: 768px)" srcset="${___HTML_LOADER_IMPORT_10___}"/> <img class="portfolio-items__img" src="${___HTML_LOADER_IMPORT_11___}" alt="Braun Landing Page - Concept"/> </picture> <h3><a href="#!">Braun Landing Page - Concept</a></h3> </div> </div> </div> </section> </main> <footer class="footer"> <div class="container"> <div class="footer-content"> <h2 class="title title-contact">Contact</h2> <p>Want to know more or just chat? <span>You are welcome!</span></p> <a class="footer-content__link" href="#!">Send message</a> <div class="footer-content__social"> <div class="social-items"> <a class="social-items__link" href="#!"> <img class="social-items__img" src="${___HTML_LOADER_IMPORT_12___}" alt="LinkedIn"/> </a> <a class="social-items__link" href="#!"> <img class="social-items__img" src="${___HTML_LOADER_IMPORT_13___}" alt="Instargam"/> </a> <a class="social-items__link" href="#!"> <img class="social-items__img" src="${___HTML_LOADER_IMPORT_14___}" alt="Behance"/> </a> <a class="social-items__link" href="#!"> <img class="social-items__img" src="${___HTML_LOADER_IMPORT_15___}" alt="Dribble"/> </a> </div> <p>Like me on <span>LinkedIn, Instagram, Behance, Dribble</span></p> </div> </div> </div> </footer> </body> </html> `));
+// Exports
+/* harmony default export */ const src = ((/* unused pure expression or super */ null && (code)));
+;// ./src/js/main.js
+
+
+
+const navigationList = document.querySelector(".navigation-list");
+const navigationLink = document.querySelectorAll(".navigation-link");
+const burger = document.querySelector(".navigation-burger-button");
+
+const scrollbarWidth = window.innerWidth - document.documentElement.clientWidth; // Высчитывание длинны скроллбара, чтобы при дальнейшем скрытии контент не сдвигался
+
+navigationLink.forEach((item) => {
+  item.addEventListener("click", (e) => {
+    navigationLink.forEach((item) => {
+      item.classList.remove("active-link");
+    });
+    e.target.classList.add("active-link");
+  });
+});
+
+burger.addEventListener("click", () => {
+  burger.classList.toggle("active-burger");
+
+  if (burger.classList.contains("active-burger")) {
+    navigationList.style.display = "flex";
+    document.body.style.marginRight = scrollbarWidth + "px";
+    document.body.style.overflowY = "hidden";
+  } else {
+    navigationList.style.display = "";
+    document.body.style.marginRight = 0;
+    document.body.style.overflowY = "visible";
+  }
+});
+
+/******/ })()
+;
